f88cb420c94cc74555a58ecba41e0ca0dbb34d54
Lars Kåre Skjørestad <lksk@RinoRabbit.local>
2016-11-23 13:45:07 +0100

    Updating happycheckin to support multiple iface

--- config/happycheckin.sh
+++ config/happycheckin.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-myip=$(ifconfig wlan0 | grep "inet addr" | awk '{print $2}')
-myhostname=$(hostname)
-message=$(echo "$myhostname" "is vibrantly checking in from" "$myip")
-curl -XPOST -H "Content-Type: application/json" http://xx/api/storehappydocument?pretty -d '{"happystatus":"above", "tags": "picheckin", "comment": "'"$message"'" }'
+sleep 10;  # Waiting and hoping for a valid IP adddress
 
+if [ "$IFACE" =  wlan0 ]; then
+  myip=$(ifconfig wlan0 | grep "inet addr" | awk '{print $2}')
+  myhostname=$(hostname)
+  message=$(echo "$myhostname" "is vibrantly checking in from" "$myip")
+  curl -XPOST -H "Content-Type: application/json" http://???/api/storehappydocument?pretty -d '{"happystatus":"above", "tags": "pich
+eckin", "comment": "'"$message"'" }'
+fi
